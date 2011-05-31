6e1ab70c3ac44dda4bf2441ee7c31a11adbadf50
Carl Boettiger <cboettig@gmail.com>
2011-05-31 15:08:45 -0700

    fixed printing display of second example

--- demos/conf_interval_ex.R
+++ demos/conf_interval_ex.R
@@ -16,10 +16,9 @@ data(simtree_lambda_dist)
 lambda <- fitContinuous(data$phy, data$data, model="lambda", hessian=TRUE)
 out <- lambda[[1]]
 ci <- sqrt(diag(solve(out$hessian)))
-
-ci
-
 o <- confidenceIntervals.pow(bm_v_lambda)
-o[["test"]][,"lambda"]
-o[["test"]][,"beta"] 
+print(ci)
+print(paste("boot lambda:", o[["test"]][,"lambda"]))
+print(paste("beta", o[["test"]][,"beta"]))
+
 
