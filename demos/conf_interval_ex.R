48af624da16f13e97398c39da6ae7c26425b1f42
Carl Boettiger <cboettig@gmail.com>
2011-05-31 13:52:15 -0700

    confidence interval comparison of methods

---
+++ demos/conf_interval_ex.R
@@ -0,0 +1,10 @@
+require(pmc)
+data(simtree_lambda_dist)
+lambda <- fitContinuous(data$phy, data$data, model=lambda, hessian=TRUE)
+out <- lambda[[1]]
+ci <- sqrt(diag(solve(out$hessian)))
+
+ci
+o[["test"]][,"lambda"]
+o[["test"]][,"beta"] 
+
