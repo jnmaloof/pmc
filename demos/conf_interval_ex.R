64a9da6ee949cc04349580ed9e750296841af59a
Carl Boettiger <cboettig@gmail.com>
2011-05-31 14:33:36 -0700

    do both geospiza and simtree

--- demos/conf_interval_ex.R
+++ demos/conf_interval_ex.R
@@ -1,4 +1,17 @@
 require(pmc)
+
+data(geospiza_lambda)
+lambda <- fitContinuous(data$phy, data$data, model="lambda", hessian=TRUE)
+out <- lambda[[1]]
+ci <- sqrt(diag(solve(out$hessian)))
+o <- confidenceIntervals.pow(bm_v_lambda)
+print(ci)
+print(paste("boot lambda:", o[["test"]][,"lambda"]))
+print(paste("beta", o[["test"]][,"beta"]))
+
+
+
+
 data(simtree_lambda_dist)
 lambda <- fitContinuous(data$phy, data$data, model="lambda", hessian=TRUE)
 out <- lambda[[1]]
