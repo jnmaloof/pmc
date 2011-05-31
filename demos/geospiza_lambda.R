39d37078b0ab8837eb4d77f1f5c2667d33523138
Carl Boettiger <cboettig@gmail.com>
2011-05-30 17:05:02 -0700

    updated geospiza_lambda example to show confidence intervals

--- demos/geospiza_lambda.R
+++ demos/geospiza_lambda.R
@@ -13,28 +13,28 @@ bm <-  fitContinuous_object(data$phy, data$data)
 lambda <- fitContinuous_object(data$phy, data$data, model="lambda")
 lambda[[1]]$lambda <- 0.6
 bm_v_lambda <- montecarlotest(bm, lambda, nboot = 1000, cpu=16)
+
+data(geospiza_lambda)
+o <- confidenceIntervals.pow(bm_v_lambda)
+# display the confidence intervals for lambda (the test model)
+o[["test"]][,"lambda"]
+
 save(list=ls(), file="geospiza_lambda.Rdat")
 
-
 ## FIGURE 1a
-#cairo_pdf("geospiza_lambda.pdf", width=4, height=4)
-
-
-require(socialR)
-
-social_plot({
+cairo_pdf("geospiza_lambda.pdf", width=4, height=4)
 par(mfrow=c(1,2))
 hist(bm_v_lambda$test_par_dist[3,], col=rgb(0,0,1,.5), border="white", breaks=15, main="", xlab="Estimated lambda")
 abline(v=lambda[[1]][3], lwd=3, lty=2, col="darkred") #True value
 text(lambda[[1]][3], 300, "True lambda", pos=2)
-#dev.off()
+dev.off()
 
 ## Figure 1c
-#cairo_pdf("geospiza_sigma.pdf", width=4, height=4)
+cairo_pdf("geospiza_sigma.pdf", width=4, height=4)
 hist(bm_v_lambda$test_par_dist[2,], col=rgb(0,0,1,.5), border="white", breaks=15, main="", xlab="Estimated sigma")
 abline(v=lambda[[1]][2], lwd=3, lty=2, col="darkred") #True value
 text(lambda[[1]][2], 300, "True sigma", pos=4)
-#dev.off()
-}, tag="phylogenetics")
+dev.off()
+
 
 
