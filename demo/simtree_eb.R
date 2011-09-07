69b8b74f673f4599dfb7d84731d838dfd4ceb79e
Carl Boettiger <cboettig@gmail.com>
2011-09-07 14:10:50 -0700

    updated eb example

--- demo/simtree_eb.R
+++ demo/simtree_eb.R
@@ -1,16 +1,17 @@
 require(pmc)
-require(TreeSim)
 ## SIMULATED EXAMPLE 
-simtree <- sim.bd.taxa(n=600, numbsim=1, lambda=1, mu=0, frac=1, complete=FALSE, stochsampling=FALSE)[[1]] 
-## Set simulated tree to acutally look like lambda~.6 data.  
-dat <- rTraitCont(exponentialchangeTree(simtree, a=-2), sigma=2)
+simtree <- sim.bd.taxa(n=600, numbsim=1, lambda=1, mu=0, frac=1, complete=FALSE, stochsampling=FALSE)[[1]][[1]] 
+
+exptree <- exponentialchangeTree(simtree, a=-2)
+
+dat <- rTraitCont(exptree, sigma=2)
 dat <- rTraitCont(simtree, sigma=2)
 dat <- rnorm(length(simtree$tip.label) )
 data<-list(phy=simtree, data=dat)
 
 # Okay, fit the models
 bm <-  fitContinuous_object(data$phy, data$data)
-eb <- fitContinuous_object(data$phy, data$data, model="eb")
+eb <- fitContinuous_object(data$phy, data$data, model="EB")
 eb[[1]]$a <- -2
 bm_v_eb <- montecarlotest(bm, eb, nboot = 1000, cpu=16)
 save(list=ls(), file="simtree_lambda_dist.Rdat")
