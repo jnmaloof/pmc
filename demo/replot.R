2c14e14f1fb09037798a7b46069169c95298dda7
Carl Boettiger <cboettig@gmail.com>
2011-09-07 14:13:49 -0700

    replot.R  autocommit

--- demo/replot.R
+++ demo/replot.R
@@ -8,16 +8,13 @@ tweet_errors(script, tags=tags)
 ###############
 
 require(pmc)
-load("early_burst.Rdat")
-png("bm_v_eb.png")
-plot(bm_v_eb)
+load("simtree_lambda_dist.Rdat")
+
+png("simtree_eb.png")
+hist(bm_v_eb$test_par_dist[3,], col=rgb(0,0,1,.5), border="white", breaks=15, main="", xlab="Estimated a")
+abline(v=eb[[1]]$a, lwd=3, lty=2, col="darkred") #True value
 dev.off()
 
-png("ou_v_eb.png")
-plot(ou_v_eb)
-dev.off()
-
-upload("bm_v_eb.png", script=script, gitaddr=gitaddr, tags=tags)
-upload("ou_v_eb.png", script=script, gitaddr=gitaddr, tags=tags)
-
+require(socialR)
+upload("simtree_eb.png", script="simtree_eb.R")
 
