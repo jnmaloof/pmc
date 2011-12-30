849646b3fb9a14043868b4100b9fc4dccea267a0
Carl Boettiger <cboettig@gmail.com>
2011-12-30 13:11:30 -0800

    documentation updates

--- R/pmc.R
+++ R/pmc.R
@@ -26,6 +26,28 @@
 #' data(geospiza)
 #' attach(geospiza)
 #' out <- pmc(geospiza.tree, geospiza.data[1], "BM", "lambda", nboot=2)
+#' 
+#' ## Ex. mixing methods from packages -- data formats handled automatically 
+#' ## Load Libraries ##
+#' require(pmc)
+#' require(TreeSim) # to simulate a sample phyologeny
+#' require(snowfall) # optional for parallelization
+#' 
+#' ## Simulate Data ##
+#' simtree <- sim.bd.taxa(n=60, numbsim=1, lambda=1, mu=0, frac=1,
+#'                        complete=FALSE, stochsampling=FALSE)[[1]][[1]] 
+#' # simulates the EB model (by tranforming the tree)
+#' dat <- rTraitCont(exponentialchangeTree(simtree, a=-0.5), sigma=5)
+#' 
+#' ## run PMC ## 
+#' sfInit(par=T, cpu=2)
+#' sfExportAll() # for parallelization
+#' ## Specify the models and the options needed.  
+#' eb_v_ou <- pmc(simtree, dat, modelA="EB", modelB="hansen", 
+#'                optionsB=list(sqrt.alpha=1, sigma=1, regimes=regimes), 
+#'                nboot=2) # small nboot just for example
+#' plot(eb_v_ou) 
+#' 
 #' @import snowfall 
 #' @import reshape
 #' @import TreeSim
--- demo/early_burst_example.R
+++ demo/early_burst_example.R
@@ -1,33 +1,33 @@
 #early_burst_example.R
+
+## Ex. mixing methods from packages -- data formats handled automatically 
+## Load Libraries ##
 require(pmc)
-require(TreeSim)
-## SIMULATED EXAMPLE 
-simtree <- sim.bd.taxa(n=60, numbsim=1, lambda=1, mu=0, frac=1, complete=FALSE, stochsampling=FALSE)[[1]][[1]] 
+require(TreeSim) # to simulate a sample phyologeny
+require(snowfall) # optional for parallelization
+
+## Simulate Data ##
+simtree <- sim.bd.taxa(n=60, numbsim=1, lambda=1, mu=0, frac=1,
+                       complete=FALSE, stochsampling=FALSE)[[1]][[1]] 
 # simulates the EB model (by tranforming the tree)
 dat <- rTraitCont(exponentialchangeTree(simtree, a=-0.5), sigma=5)
-## run PMC
-sfInit(par=T, cpu=4)
-sfExportAll()
-ou_v_eb <- pmc(simtree, dat, modelA="EB", modelB="OU", nboot=4)
-#plot(ou_v_eb)
 
+## run PMC ## 
+sfInit(par=T, cpu=2)
+sfExportAll() # for parallelization
+## Specify the models and the options needed.  
+eb_v_ou <- pmc(simtree, dat, modelA="EB", modelB="hansen", 
+               optionsB=list(sqrt.alpha=1, sigma=1, regimes=regimes), 
+               nboot=2) # small nboot just for example
+plot(eb_v_ou) 
+
+
+## examples of more basic functions 
 ouch <- format_data(simtree, dat)
 ape <- format_data(ouch$tree, ouch$dat)
 A <- pmc_fit(ape$tree, ape$data, model="BM")
-
-
 regimes <- format_data(simtree,dat)$noregimes
 B <- pmc_fit(simtree, dat, model="hansen", options=list(sqrt.alpha=1, sigma=1, regimes=regimes))
 
-eb_v_ou <- pmc(simtree, dat, modelA="EB", modelB="hansen", optionsB=list(sqrt.alpha=1, sigma=1, regimes=regimes), nboot=2)
-plot(eb_v_ou)
-
-
-
-require(TreeSim)
-simtree <- sim.bd.taxa(n=5, numbsim=1, lambda=1, mu=0, frac=1, complete=FALSE, stochsampling=FALSE)[[1]][[1]] 
-ouchtree <- ape2ouch(simtree)
-apetree <- convert(ouchtree)
-apetree
 
 
